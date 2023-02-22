5383514fceb88ca5c9aea8303d84d836c03e9013
Mike Klein <mtklein@gmail.com>
2023-02-22 22:52:54 +0800

    basic mutate() test

--- test.c
+++ test.c
@@ -33,8 +33,30 @@ static void test_triple(void) {
     free(p);
 }
 
+static void test_mutate(void) {
+    struct Builder *b = builder();
+    {
+        int x = load(b, 0);
+        mutate(b, &x, fmul(b, x, splat(b, 3.0f)));
+        store(b,0,x);
+    }
+    struct Program *p = compile(b);
+
+    float f[] = {1.0f,2.0f,3.0f,4.0f,5.0f,6.0f};
+    execute(p, len(f), NULL, (float*[]){f});
+
+    assert_eq(f[0],  3.0f);
+    assert_eq(f[1],  6.0f);
+    assert_eq(f[2],  9.0f);
+    assert_eq(f[3], 12.0f);
+    assert_eq(f[4], 15.0f);
+    assert_eq(f[5], 18.0f);
+    free(p);
+}
+
 int main(void) {
     test_nothing();
     test_triple();
+    test_mutate();
     return 0;
 }
