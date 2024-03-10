41ed0b33ac93960303a27a1d3d771f4b4f432b25
Mike Klein <mtklein@gmail.com>
2024-03-09 23:26:09 -0600

    style, especially typedefs

--- twvm.c
+++ twvm.c
@@ -7,39 +7,39 @@
 #define K 4
 #define vector(T) T __attribute__((vector_size(sizeof(T) * K)))
 
-typedef union {
+union Val {
     vector(float) f;
     vector(int)   i;
-} V32;
+};
 
-typedef struct PInst {
-    int (*fn)(struct PInst const *ip, V32 *v, int end, void *ptr[]);
+struct PInst {
+    void (*fn)(struct PInst const *ip, union Val *v, int end, void *ptr[]);
     int   x,y,z;  // Relative to this instruction (almost always negative).
     union { int ptr; float imm; };
-} PInst;
+};
 
-typedef enum { CONSTANT,UNIFORM,VARYING } Shape;
+enum Shape { CONSTANT,UNIFORM,VARYING };
 
-typedef struct BInst {
-    int (*fn)(struct PInst const *ip, V32 *v, int end, void *ptr[]);
+struct BInst {
+    void (*fn)(struct PInst const *ip, union Val *v, int end, void *ptr[]);
     int   x,y,z;  // Absolute into b->inst, with id=0 predefined as a phony value (N/A).
     union { int ptr; float imm; };
 
-    Shape shape   :  2;
-    _Bool live    :  1;
-    int   ptr_gen : 29;
-    int   id;
-} BInst;
+    enum Shape shape   :  2;
+    _Bool      live    :  1;
+    int        ptr_gen : 29;
+    int        id;
+};
 
-typedef struct Builder {
-    BInst       *inst;
-    int         *ptr_gen;
-    int          insts,ptrs;
-    struct hash *cse;
-} Builder;
+struct Builder {
+    struct BInst *inst;
+    int          *ptr_gen;
+    int           insts,ptrs;
+    struct hash  *cse;
+};
 
-Builder* builder(int ptrs) {
-    Builder *b = calloc(1, sizeof *b);
+struct Builder* builder(int ptrs) {
+    struct Builder *b = calloc(1, sizeof *b);
     // A phony instruction at id=0 lets us assume that every BInst's inputs (x,y,z) always exist.
     b->inst    = calloc(1, sizeof *b->inst);
     b->insts   = 1;
@@ -48,30 +48,25 @@ Builder* builder(int ptrs) {
     return b;
 }
 
-#if defined(__clang__) && !defined(__wasm__)
-    #define next __attribute__((musttail)) return ip[1].fn(ip+1,v+1,end,ptr)
-#else
-    #define next                           return ip[1].fn(ip+1,v+1,end,ptr)
-#endif
 
-#define stage(name) int name##_(PInst const *ip, V32 *v, int end, void *ptr[])
+#define defn(name) static void name##_(struct PInst const *ip, union Val *v, int end, void *ptr[])
+#define next ip[1].fn(ip+1,v+1,end,ptr); return
 
-static stage(done) {
+defn(done) {
     (void)ip;
     (void)v;
     (void)end;
     (void)ptr;
-    return 0;
 }
 
-static int constant_fold(Builder *b, BInst inst) {
+static int constant_fold(struct Builder *b, struct BInst inst) {
     if (inst.shape == CONSTANT && (inst.x || inst.y || inst.z)) {
-        V32 v[4] = {
+        union Val v[4] = {
             {{b->inst[inst.x].imm}},
             {{b->inst[inst.y].imm}},
             {{b->inst[inst.z].imm}},
         };
-        PInst ip[] = {
+        struct PInst ip[] = {
             {.fn=inst.fn, .x=-3, .y=-2, .z=-1, .ptr=inst.ptr},
             {.fn=done_},
         };
@@ -81,14 +76,14 @@ static int constant_fold(Builder *b, BInst inst) {
     return 0;
 }
 
-typedef struct {
-    Builder const *b;
-    BInst   const *want;
-    int            id, unused;
-} MatchCtx;
+struct MatchCtx {
+    struct Builder const *b;
+    struct BInst   const *want;
+    int                   id, unused;
+};
 
 static _Bool cse_match(int val, void *vctx) {
-    MatchCtx *ctx = vctx;
+    struct MatchCtx *ctx = vctx;
     if (0 == __builtin_memcmp(ctx->want, ctx->b->inst+val, sizeof *ctx->want)) {
         ctx->id = val;
         return 1;
@@ -106,7 +101,7 @@ static unsigned fnv1a(void const *v, size_t len) {
     return hash;
 }
 
-static int push_(Builder *b, BInst inst) {
+static int push_(struct Builder *b, struct BInst inst) {
     assert(inst.x < b->insts);
     assert(inst.y < b->insts);
     assert(inst.z < b->insts);
@@ -120,7 +115,7 @@ static int push_(Builder *b, BInst inst) {
     }
 
     unsigned const hash = fnv1a(&inst, sizeof inst);
-    for (MatchCtx ctx = {b,.want=&inst}; hash_lookup(b->cse, hash, cse_match, &ctx); ) {
+    for (struct MatchCtx ctx = {b,.want=&inst}; hash_lookup(b->cse, hash, cse_match, &ctx); ) {
         return ctx.id;
     }
 
@@ -135,48 +130,49 @@ static int push_(Builder *b, BInst inst) {
     }
     return id;
 }
-#define push(b,...) push_(b, (BInst){__VA_ARGS__})
+#define push(b,...) push_(b, (struct BInst){__VA_ARGS__})
 
 // When op(x,y)==op(y,x), using sort() instead of push() will canonicalize, allowing more CSE.
-static int sort_(Builder *b, BInst inst) {
+static int sort_(struct Builder *b, struct BInst inst) {
     if (inst.x > inst.y) {
-        int tmp = inst.y;
-        inst.y  = inst.x;
-        inst.x  = tmp;
+        int y0 = inst.y;
+        inst.y = inst.x;
+        inst.x = y0;
     }
     return push_(b,inst);
 }
-#define sort(b,...) sort_(b, (BInst){__VA_ARGS__})
+#define sort(b,...) sort_(b, (struct BInst){__VA_ARGS__})
 
-static stage(thread_id) {
-#if K == 4
-    vector(float) iota = {0,1,2,3};
-#endif
-    if (end & (K-1)) { v->f = (float)end - 1 + iota; }
-    else             { v->f = (float)end - K + iota; }
+defn(thread_id) {
+    union {
+        float         arr[8];
+        vector(float) vec;
+    } iota = {{0,1,2,3,4,5,6,7}};
+    if (end & (K-1)) { v->f = (float)end - 1 + iota.vec; }
+    else             { v->f = (float)end - K + iota.vec; }
     next;
 }
-int thread_id(Builder *b) { return push(b, .fn=thread_id_, .shape=VARYING); }
+int thread_id(struct Builder *b) { return push(b, .fn=thread_id_, .shape=VARYING); }
 
-static stage(splat) {
+defn(splat) {
     v->f = ( (vector(float)){0} + 1 ) * ip->imm;
     next;
 }
-int splat(Builder *b, float imm) { return push(b, .fn=splat_, .imm=imm); }
+int splat(struct Builder *b, float imm) { return push(b, .fn=splat_, .imm=imm); }
 
-static stage(load_uniform) {
+defn(load_uniform) {
     float const *p = ptr[ip->ptr],
                 ix = v[ip->x].f[0];
     v->f = ( (vector(float)){0} + 1 ) * p[(int)ix];
     next;
 }
-static stage(load_contiguous) {
+defn(load_contiguous) {
     float const *p = ptr[ip->ptr];
     if (end & (K-1)) { __builtin_memcpy(v, p + end - 1,   sizeof(float)); }
     else             { __builtin_memcpy(v, p + end - K, K*sizeof(float)); }
     next;
 }
-static stage(load_gather) {
+defn(load_gather) {
     float const   *p = ptr[ip->ptr];
     vector(float) ix = v[ip->x].f;
     for (int i = 0; i < K; i++) {
@@ -184,7 +180,7 @@ static stage(load_gather) {
     }
     next;
 }
-int load(Builder *b, int ptr, int ix) {
+int load(struct Builder *b, int ptr, int ix) {
     assert(ptr < b->ptrs);
     int const ptr_gen = b->ptr_gen[ptr];
     if (b->inst[ix].shape <= UNIFORM) {
@@ -196,13 +192,13 @@ int load(Builder *b, int ptr, int ix) {
     return push(b, .fn=load_gather_, .ptr=ptr, .x=ix, .shape=VARYING, .ptr_gen=ptr_gen);
 }
 
-static stage(store) {
+defn(store) {
     float *p = ptr[ip->ptr];
     if (end & (K-1)) { __builtin_memcpy(p + end - 1, v+ip->x,   sizeof(float)); }
     else             { __builtin_memcpy(p + end - K, v+ip->x, K*sizeof(float)); }
     next;
 }
-void store(Builder *b, int ptr, int x) {
+void store(struct Builder *b, int ptr, int x) {
     assert(ptr < b->ptrs);
     push(b, .fn=store_, .ptr=ptr, .x=x, .shape=VARYING, .live=1);
     b->ptr_gen[ptr]++;
@@ -214,18 +210,18 @@ void store(Builder *b, int ptr, int x) {
     #pragma GCC diagnostic ignored "-Wvector-conversion"  // vector(long) != vector(int) somehow?
 #endif
 
-static stage(fadd) { v->f = v[ip->x].f +  v[ip->y].f             ; next; }
-static stage(fsub) { v->f = v[ip->x].f -  v[ip->y].f             ; next; }
-static stage(fmul) { v->f = v[ip->x].f *  v[ip->y].f             ; next; }
-static stage(fdiv) { v->f = v[ip->x].f /  v[ip->y].f             ; next; }
-static stage(fmad) { v->f = v[ip->x].f *  v[ip->y].f + v[ip->z].f; next; }
-static stage(feq ) { v->i = v[ip->x].f == v[ip->y].f             ; next; }
-static stage(flt ) { v->i = v[ip->x].f <  v[ip->y].f             ; next; }
-static stage(fle ) { v->i = v[ip->x].f <= v[ip->y].f             ; next; }
-static stage(band) { v->i = v[ip->x].i &  v[ip->y].i             ; next; }
-static stage(bor ) { v->i = v[ip->x].i |  v[ip->y].i             ; next; }
-static stage(bxor) { v->i = v[ip->x].i ^  v[ip->y].i             ; next; }
-static stage(bsel) {
+defn(fadd) { v->f = v[ip->x].f +  v[ip->y].f             ; next; }
+defn(fsub) { v->f = v[ip->x].f -  v[ip->y].f             ; next; }
+defn(fmul) { v->f = v[ip->x].f *  v[ip->y].f             ; next; }
+defn(fdiv) { v->f = v[ip->x].f /  v[ip->y].f             ; next; }
+defn(fmad) { v->f = v[ip->x].f *  v[ip->y].f + v[ip->z].f; next; }
+defn(feq ) { v->i = v[ip->x].f == v[ip->y].f             ; next; }
+defn(flt ) { v->i = v[ip->x].f <  v[ip->y].f             ; next; }
+defn(fle ) { v->i = v[ip->x].f <= v[ip->y].f             ; next; }
+defn(band) { v->i = v[ip->x].i &  v[ip->y].i             ; next; }
+defn(bor ) { v->i = v[ip->x].i |  v[ip->y].i             ; next; }
+defn(bxor) { v->i = v[ip->x].i ^  v[ip->y].i             ; next; }
+defn(bsel) {
     v->i = ( v[ip->x].i & v[ip->y].i)
          | (~v[ip->x].i & v[ip->z].i);
     next;
@@ -233,31 +229,31 @@ static stage(bsel) {
 
 #pragma GCC diagnostic pop
 
-int fadd(Builder *b, int x, int y) {
+int fadd(struct Builder *b, int x, int y) {
     if (b->inst[x].fn==fmul_) { return push(b, .fn=fmad_, .x=b->inst[x].x, .y=b->inst[x].y, .z=y); }
     if (b->inst[y].fn==fmul_) { return push(b, .fn=fmad_, .x=b->inst[y].x, .y=b->inst[y].y, .z=x); }
     return sort(b, .fn=fadd_, .x=x, .y=y);
 }
 
-int fsub(Builder *b, int x, int y       ) { return push(b, .fn=fsub_, .x=x, .y=y      ); }
-int fmul(Builder *b, int x, int y       ) { return sort(b, .fn=fmul_, .x=x, .y=y      ); }
-int fdiv(Builder *b, int x, int y       ) { return push(b, .fn=fdiv_, .x=x, .y=y      ); }
-int feq (Builder *b, int x, int y       ) { return sort(b, .fn=feq_ , .x=x, .y=y      ); }
-int flt (Builder *b, int x, int y       ) { return push(b, .fn=flt_ , .x=x, .y=y      ); }
-int fle (Builder *b, int x, int y       ) { return push(b, .fn=fle_ , .x=x, .y=y      ); }
-int band(Builder *b, int x, int y       ) { return sort(b, .fn=band_, .x=x, .y=y      ); }
-int bor (Builder *b, int x, int y       ) { return sort(b, .fn=bor_ , .x=x, .y=y      ); }
-int bxor(Builder *b, int x, int y       ) { return sort(b, .fn=bxor_, .x=x, .y=y      ); }
-int bsel(Builder *b, int x, int y, int z) { return push(b, .fn=bsel_, .x=x, .y=y, .z=z); }
+int fsub(struct Builder *b, int x, int y       ) { return push(b, .fn=fsub_, .x=x, .y=y      ); }
+int fmul(struct Builder *b, int x, int y       ) { return sort(b, .fn=fmul_, .x=x, .y=y      ); }
+int fdiv(struct Builder *b, int x, int y       ) { return push(b, .fn=fdiv_, .x=x, .y=y      ); }
+int feq (struct Builder *b, int x, int y       ) { return sort(b, .fn=feq_ , .x=x, .y=y      ); }
+int flt (struct Builder *b, int x, int y       ) { return push(b, .fn=flt_ , .x=x, .y=y      ); }
+int fle (struct Builder *b, int x, int y       ) { return push(b, .fn=fle_ , .x=x, .y=y      ); }
+int band(struct Builder *b, int x, int y       ) { return sort(b, .fn=band_, .x=x, .y=y      ); }
+int bor (struct Builder *b, int x, int y       ) { return sort(b, .fn=bor_ , .x=x, .y=y      ); }
+int bxor(struct Builder *b, int x, int y       ) { return sort(b, .fn=bxor_, .x=x, .y=y      ); }
+int bsel(struct Builder *b, int x, int y, int z) { return push(b, .fn=bsel_, .x=x, .y=y, .z=z); }
 
-int fgt(Builder *b, int x, int y) { return flt(b,y,x); }
-int fge(Builder *b, int x, int y) { return fle(b,y,x); }
+int fgt(struct Builder *b, int x, int y) { return flt(b,y,x); }
+int fge(struct Builder *b, int x, int y) { return fle(b,y,x); }
 
-static stage(mutate) {
+defn(mutate) {
     v[ip->x] = v[ip->y];
     next;
 }
-void mutate(Builder *b, int *var, int val) {
+void mutate(struct Builder *b, int *var, int val) {
     push(b, .fn=mutate_, .x=*var, .y=val, .live=1);
 
     // Forget all CSE entries when anything mutates.  (TODO: kind of a big hammer)
@@ -265,7 +261,7 @@ void mutate(Builder *b, int *var, int val) {
     b->cse = NULL;
 }
 
-static stage(jump) {
+defn(jump) {
     vector(int) const cond = v[ip->y].i;
 #if __has_builtin(__builtin_reduce_min)
     int const any = __builtin_reduce_min(cond);
@@ -282,20 +278,20 @@ static stage(jump) {
     }
     next;
 }
-void jump(Builder *b, int dst, int cond) { push(b, .fn=jump_, .x=dst, .y=cond, .live=1); }
+void jump(struct Builder *b, int dst, int cond) { push(b, .fn=jump_, .x=dst, .y=cond, .live=1); }
 
-typedef struct Program {
-    int   insts,loop;
-    PInst inst[];
-} Program;
+struct Program {
+    int          insts,loop;
+    struct PInst inst[];
+};
 
-Program* compile(Builder *b) {
+struct Program* compile(struct Builder *b) {
     push(b, .fn=done_, .shape=VARYING, .live=1);
 
     // Dead code elimination: the inputs of live instructions are live, and anything else is dead.
     // Marking dead instructions with fn=NULL handles the phony id=0 instruction naturally.
     int live = 0;
-    for (BInst *inst = b->inst + b->insts; inst --> b->inst;) {
+    for (struct BInst *inst = b->inst + b->insts; inst --> b->inst;) {
         if (inst->live) {
             b->inst[inst->x].live = 1;
             b->inst[inst->y].live = 1;
@@ -306,16 +302,16 @@ Program* compile(Builder *b) {
         live += (inst->fn != NULL);
     }
 
-    Program *p = calloc(1, sizeof *p + (size_t)live * sizeof *b->inst);
+    struct Program *p = calloc(1, sizeof *p + (size_t)live * sizeof *b->inst);
 
     for (int varying = 0; varying < 2; varying++) {
         if (varying) {
             p->loop = p->insts;
         }
-        for (BInst *inst = b->inst; inst < b->inst + b->insts; inst++) {
+        for (struct BInst *inst = b->inst; inst < b->inst + b->insts; inst++) {
             if (inst->fn && (inst->shape == VARYING) == varying) {
                 inst->id = p->insts++;
-                p->inst[inst->id] = (PInst) {
+                p->inst[inst->id] = (struct PInst) {
                     .fn  = inst->fn,
                     .x   = b->inst[inst->x].id - inst->id,
                     .y   = b->inst[inst->y].id - inst->id,
@@ -334,11 +330,11 @@ Program* compile(Builder *b) {
     return p;
 }
 
-void execute(Program const *p, int n, void *ptr[]) {
-    V32 *val = calloc((size_t)p->insts, sizeof *val);
+void execute(struct Program const *p, int n, void *ptr[]) {
+    union Val *val = calloc((size_t)p->insts, sizeof *val);
 
-    PInst const *ip = p->inst,  *loop = ip + p->loop;
-    V32          *v = val    , *vloop =  v + p->loop;
+    struct PInst const *ip = p->inst,  *loop = ip + p->loop;
+    union Val           *v = val    , *vloop =  v + p->loop;
 
     for (int i = 0; i < n/K*K; i += K) { ip->fn(ip,v,i+K,ptr); ip = loop; v = vloop; }
     for (int i = n/K*K; i < n; i += 1) { ip->fn(ip,v,i+1,ptr); ip = loop; v = vloop; }
@@ -347,7 +343,7 @@ void execute(Program const *p, int n, void *ptr[]) {
 }
 
 static void test_constant_prop(void) {
-    Builder *b = builder(0);
+    struct Builder *b = builder(0);
     int x = splat(b,2.0f),
         y = fmul (b,x,x);
     expect(b->inst[y].fn  == splat_ && b->inst[y].imm == 4.0f);
@@ -355,14 +351,14 @@ static void test_constant_prop(void) {
 }
 
 static void test_dead_code_elimination(void) {
-    Builder *b = builder(1);
+    struct Builder *b = builder(1);
     {
         int live = splat(b,2.0f),
             dead = splat(b,4.0f);
         (void)dead;
         store(b,0,live);
     }
-    Program *p = compile(b);
+    struct Program *p = compile(b);
     expect(p->insts == 3);
     expect(p->inst[0].fn == splat_ && p->inst[0].imm == 2.0f);
     expect(p->inst[1].fn == store_);
@@ -378,14 +374,14 @@ static void test_fmad(void) {
             z = fadd(b,y,splat(b,3.0f));
         store(b,0,z);
     }
-    Program *p = compile(b);
+    struct Program *p = compile(b);
     expect(p->insts == 5);
     expect(p->inst[2].fn == fmad_);
     free(p);
 }
 
 static void test_loop_hoisting(void) {
-    Builder *b = builder(1);
+    struct Builder *b = builder(1);
     {
         int x = load(b,0,thread_id(b)),
             y = load(b,0,splat(b,0.0f)),
@@ -393,7 +389,7 @@ static void test_loop_hoisting(void) {
             w = fmul(b,x,z);
         store(b,0,w);
     }
-    Program *p = compile(b);
+    struct Program *p = compile(b);
     expect(p->insts == 8);
     expect(p->loop  == 4);
     expect(p->inst[0].fn == splat_ && p->inst[0].imm == 0.0f);
@@ -407,7 +403,7 @@ static void test_loop_hoisting(void) {
 }
 
 static void test_cse(void) {
-    Builder *b = builder(1);
+    struct Builder *b = builder(1);
     {
         int x = load(b,0,thread_id(b)),
             y = fmul(b,x,x),
@@ -417,7 +413,7 @@ static void test_cse(void) {
     free(compile(b));
 }
 static void test_more_cse(void) {
-    Builder *b = builder(0);
+    struct Builder *b = builder(0);
     {
         int x = splat(b,2.0f),
             y = splat(b,2.0f);
@@ -426,7 +422,7 @@ static void test_more_cse(void) {
     free(compile(b));
 }
 static void test_no_cse(void) {
-    Builder *b = builder(1);
+    struct Builder *b = builder(1);
     {
         int x = load(b,0,thread_id(b)),
             y = fmul(b,x,x);
@@ -440,7 +436,7 @@ static void test_no_cse(void) {
 // TODO: does mutate() make sense on values which have already been CSE'd?
 // Perhaps this indicates we do need explicit variables instead of mutate().
 static void test_also_no_cse(void) {
-    Builder *b = builder(1);
+    struct Builder *b = builder(1);
     if ((0)) {
         int x = load(b,0,thread_id(b)),
             y = fmul(b,x,x),
@@ -453,8 +449,8 @@ static void test_also_no_cse(void) {
 }
 
 static void test_cse_sort(void) {
-    Builder *b = builder(1);
-     {
+    struct Builder *b = builder(1);
+    {
         int x = load (b,0,thread_id(b)),
             c = splat(b,2.0f),
             y = fmul (b,x,c),
@@ -466,8 +462,8 @@ static void test_cse_sort(void) {
     free(compile(b));
 }
 static void test_cse_no_sort(void) {
-    Builder *b = builder(1);
-     {
+    struct Builder *b = builder(1);
+    {
         int x = load (b,0,thread_id(b)),
             c = splat(b,2.0f),
             y = fdiv (b,x,c),
@@ -480,7 +476,7 @@ static void test_cse_no_sort(void) {
 }
 
 static void test_load_cse(void) {
-    Builder *b = builder(2);
+    struct Builder *b = builder(2);
     {
         int x = load(b,0, thread_id(b)),
             y = load(b,0, thread_id(b)),
